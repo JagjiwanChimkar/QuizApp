2e26dd05f4fba5b108a5463ff8aeb2e5d493c435
saikumar221 <saikumarreddy221@gmail.com>
2020-10-07 04:58:59 +0530

    UI Improvements Highscores page

--- highscores.js
+++ highscores.js
@@ -1,8 +1,9 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+
+  highScoresList.innerHTML = highScores
+  .map( (score,index) => {
+    return `<tr><td> ${index + 1} </td> <td>${score.name} </td> <td> ${score.score}</td> </tr>`;
   })
   .join("");
